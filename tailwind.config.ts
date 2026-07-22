c8b882dcb5b696cde382250baa34f885290fce72
supermat26 <e2sbatteri@gmail.com>
2026-07-22 16:26:40 +0200

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        red: {
+          600: '#E31837', // REMA 1000 rød
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
